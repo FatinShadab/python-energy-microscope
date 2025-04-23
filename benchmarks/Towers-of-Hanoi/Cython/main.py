e1ba5355ab785fc6572d701fa5cfcc95777d4fec
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2025-04-23 12:46:48 +0600

    Update main.py
    added time module for time benchmark

--- benchmarks/Towers-of-Hanoi/Cython/main.py
+++ benchmarks/Towers-of-Hanoi/Cython/main.py
@@ -4,17 +4,38 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
 
 from energy_module.decorator import measure_energy_to_csv
+from time_modules.decorator import measure_time_to_csv
 from raw import towers_of_hanoi
 from input import __default__
 import argparse
 
-@measure_energy_to_csv(n=__default__["hanoi"]["test_n"], csv_filename="hanoi_cython")
-def driver(n: int) -> None:
+
+def main(n: int) -> None:
     try:
         print(f"Number of disks: {n}")
         towers_of_hanoi(n, "A", "B", "C")
     except ValueError as e:
         print(f"Error: {e}")
+        
+@measure_energy_to_csv(n=__default__["hanoi"]["test_n"], csv_filename="hanoi_cython")
+def run_energy_benchmark(n: int) -> None:
+    """
+    Driver function to run the Towers of Hanoi solution and measure energy consumption.
+    
+    Args:
+        n (int): Number of disks.
+    """
+    main(n)
+
+@measure_time_to_csv(n=__default__["hanoi"]["test_n"], csv_filename="hanoi_cython")
+def run_time_benchmark(n: int) -> None:
+    """
+    Driver function to run the Towers of Hanoi solution and measure time consumption.
+    
+    Args:
+        n (int): Number of disks.
+    """
+    main(n)
 
 
 if __name__ == "__main__":
@@ -28,4 +49,5 @@ if __name__ == "__main__":
 
     # Get the number of disks from the arguments or use the default value
     n = args.num_disks
-    driver(n)
+    run_energy_benchmark(n)
+    run_time_benchmark(n)
