b15595429b995785845ca72f6db8ae999f891c69
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2025-04-25 00:12:05 +0600

    added benchmark decorators

--- benchmarks/Pi-Digits/PyPy/main.py
+++ benchmarks/Pi-Digits/PyPy/main.py
@@ -1,3 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+
+from energy_module.decorator import measure_energy_to_csv
+from time_modules.decorator import measure_time_to_csv
+from input import __default__
+
 import math
 
 def compute_pi_gauss_legendre(iterations: int = 10) -> float:
@@ -29,9 +37,20 @@ def compute_pi_gauss_legendre(iterations: int = 10) -> float:
     # Compute final Pi approximation
     return ((a + b) ** 2) / (4 * t)
 
+@measure_energy_to_csv(n=__default__["pi_digits"]["test_n"], csv_filename="pi_digits_pypy")
+def run_energy_benchmark(iterations: int) -> None:
+    pi_approx : float = compute_pi_gauss_legendre(iterations)
+    print(f"Computed Pi: {pi_approx}")
+
+@measure_time_to_csv(n=__default__["pi_digits"]["test_n"], csv_filename="pi_digits_pypy")
+def run_time_benchmark(iterations: int) -> None:
+    pi_approx : float = compute_pi_gauss_legendre(iterations)
+    print(f"Computed Pi: {pi_approx}")
+
 
 if __name__ == "__main__":
-    ITERATIONS = 20  # Adjust for precision
-    pi_approx: float = compute_pi_gauss_legendre(ITERATIONS)
-
-    print(f"Computed Pi: {pi_approx}")
+    ITERATIONS = __default__["pi_digits"]["iterations"]
+    
+    # Run benchmarks
+    run_energy_benchmark(ITERATIONS)
+    run_time_benchmark(ITERATIONS)
