8d0ab0f0f57a34cadd9c7eac2091d420d42c3fc2
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2025-04-24 17:11:48 +0600

    Update main.py

--- benchmarks/N-Queens/Ctypes/main.py
+++ benchmarks/N-Queens/Ctypes/main.py
@@ -1,4 +1,11 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+
+from energy_module.decorator import measure_energy_to_csv
+from time_modules.decorator import measure_time_to_csv
+from input import __default__
+
 import ctypes
 from ctypes import POINTER, c_int, byref
 
@@ -30,12 +37,27 @@ def solve_n_queens(n):
 
     return result
 
-# Example
-if __name__ == "__main__":
-    n = 8
+def main(n):
     solutions = solve_n_queens(n)
     print(f"Total solutions: {len(solutions)}")
     for sol in solutions:
         for row in sol:
             print(" ".join("Q" if x else "." for x in row))
         print()
+        
+@measure_energy_to_csv(n=__default__["n-queens"]["test_n"], csv_filename="n_queens_ctypes")
+def run_energy_benchmark(n: int) -> None:
+    main(n)
+
+@measure_time_to_csv(n=__default__["n-queens"]["test_n"], csv_filename="n_queens_ctypes")
+def run_time_benchmark(n: int) -> None:
+    main(n)
+
+
+if __name__ == "__main__":
+    N = __default__["n-queens"]["n"]
+   
+    # Run the energy benchmark
+    run_energy_benchmark(N)
+    # Run the time benchmark
+    run_time_benchmark(N)
