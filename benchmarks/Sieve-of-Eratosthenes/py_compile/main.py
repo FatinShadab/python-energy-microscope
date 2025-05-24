3df61b546fbacd18434a1d50436cc9688b5e0289
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2025-05-24 20:23:58 +0600

    updated py_compile implementation

--- benchmarks/Sieve-of-Eratosthenes/py_compile/main.py
+++ benchmarks/Sieve-of-Eratosthenes/py_compile/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../")))
 
 from energy_module.decorator import measure_energy_to_csv
 from time_modules.decorator import measure_time_to_csv
